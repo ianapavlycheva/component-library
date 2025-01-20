42039e08b96dddebda5fe2e6d0d4991d40176dde
ianapavlycheva <62348602+ianapavlycheva@users.noreply.github.com>
2025-01-20 18:02:33 +1100

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import Menu from "./components/Menu/index"
+// import Star from "./components/Star"
+
+function App() {
+  return (
+    <>
+        <h1>Your components go here</h1>
+    </>
+  )
+}
+
+export default App
